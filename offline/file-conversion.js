3bf3ab9043433da2dfb4f4be7e697eaa9e201d86
Adriana Trif <trif.corina.adriana@gmail.com>
2020-03-10 10:03:56 +0200

    Update conversion methods

--- offline/file-conversion.js
+++ offline/file-conversion.js
@@ -1,4 +1,4 @@
-export const getBlob(file) {
+export function getBlobAsText(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.addEventListener('loadend', (e) => {
@@ -9,7 +9,7 @@ export const getBlob(file) {
   });
 }
 
-export const getArrayBuffer(blob) {
+export function getArrayBuffer(blob) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.addEventListener('loadend', (e) => {
@@ -18,4 +18,29 @@ export const getArrayBuffer(blob) {
     reader.addEventListener('error', reject);
     reader.readAsArrayBuffer(blob);
   });
+}
+
+export function getBlob(fileUrl) {
+  //** Using XHR */
+  // return new Promise((resolve, reject) => {
+  //   let xhr = new XMLHttpRequest();
+  //   let blob;
+  //   xhr.open('GET', blobUrl, true);
+  //   xhr.responseType = 'blob';
+  //   xhr.addEventListener('load', function () {
+  //     if (xhr.status === 200) {
+  //       blob = xhr.response;
+  //       resolve(blob);
+  //     }
+  //   });
+  //   xhr.send();
+  // });
+
+  //** Using fetch */
+  let response = await fetch(fileUrl);
+  return response.blob();
+}
+
+export function getFileUrl(file) {
+  return window.URL.createObjectURL(file);
 }
