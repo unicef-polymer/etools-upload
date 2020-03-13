d4f257236f9e9c1896c85f196a8ac16299025d7e
Adriana Trif <trif.corina.adriana@gmail.com>
2020-03-13 10:27:33 +0200

    d.ts

--- offline/dexie-operations.d.ts
+++ offline/dexie-operations.d.ts
@@ -1,7 +1,7 @@
-declare function storeFileInDexie(fileInfo: any): Promise<any>;
+export declare function storeFileInDexie(fileInfo: any): Promise<any>;
 
-declare function getFileFromDexieById(id: string): Promise<any>;
+export declare function getFileFromDexieById(id: string): Promise<any>;
 
-declare function getFilesFromDexieByIds(ids: string[]): Promise<any[]>;
+export declare function getFilesFromDexieByIds(ids: string[]): Promise<any[]>;
 
-declare function deleteFileFromDexie(id: string): Promise<any>; //TODO test if more specific return type can be provided
+export declare function deleteFileFromDexie(id: string): Promise<any>; //TODO test if more specific return type can be provided
--- upload-helper.d.ts
+++ upload-helper.d.ts
@@ -16,7 +16,8 @@ export interface OperationResponse {
   success: any[],
   error: any[]
 }
-declare function upload(config: UploadConfig, rawFile: File | Blob, filename: string): Promise<any>;
-declare function uploadAllFilesSequentially(config: UploadConfig, files: SequentialUploadFiles[]): Promise<OperationResponse>;
-declare function uploadFileStoredInDexie(config: UploadConfig, id: string): Promise<OperationResponse>;
+
+export declare function upload(config: UploadConfig, rawFile: File | Blob, filename: string): Promise<any>;
+export declare function uploadAllFilesSequentially(config: UploadConfig, files: SequentialUploadFiles[]): Promise<OperationResponse>;
+export declare function uploadFileStoredInDexie(config: UploadConfig, id: string): Promise<OperationResponse>;
 
