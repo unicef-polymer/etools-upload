5b3efa341d01af5a3eba5b93674fbb2d2afee3be
Adriana Trif <trif.corina.adriana@gmail.com>
2021-03-12 13:56:00 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export {EtoolsUpload} from './EtoolsUpload.js';
 export {EtoolsUploadMulti} from './EtoolsUploadMulti.js';
 export {uploadFileStoredInDexie} from './upload-helper.js';
+export {getFileFromDexieById, getFilesFromDexieByIds, deleteFileFromDexie} from './offline/dexie-operations.js';
